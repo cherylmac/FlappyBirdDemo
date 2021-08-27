4f2d4775917d7781540fd730c8ec3341777ab3b3
Cheryl McIntyre <cherylmacdev@gmail.com>
2021-08-27 14:23:34 +0100

    Add project files.

---
+++ FlappyBirdDemo.Web/Models/BirdModel.cs
@@ -0,0 +1,27 @@
+﻿namespace FlappyBirdDemo.Web.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+        public int JumpStrength { get; private set; } = 50;
+
+        public bool IsOnGround()
+        {
+            return DistanceFromGround <= 0;
+        }
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+
+        public void Jump()
+        {
+            if (DistanceFromGround <= 530)
+            {
+                DistanceFromGround += JumpStrength;
+            }
+            
+        }
+    }
+}
